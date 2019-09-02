cbd8e3e8c4ffd5135b1e7a9c7c4e87fa2584fdab
KenepAI <dijonkock@gmail.com>
2019-09-02 16:38:23 +0200

    7. Add a custom classifier on top of the MobileNet predictions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let net;
 const webcamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create(); 
 
 //  webcam setup function
 async function setupWebcam() {
@@ -30,23 +31,38 @@ async function app() {
   net = await mobilenet.load(); 
   console.log('Sucessfully loaded model');
 
-    // Make a prediction through the model on our image.
-    /* 
-    remove the prediction through the image and 
-    instead create an infinite loop which makes predictions through the webcam element.
-    const imgEl = document.getElementById('img');
-    const result = await net.classify(imgEl);
-    console.log(result);
-    */
+   // Reads an image from the webcam and associates it with a specific class
+  // index.
+  const addExample = classId => {
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(webcamElement, 'conv_preds');
+
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+  };
+
+  // When clicking a button, add an example for that class.
+  document.getElementById('class-a').addEventListener('click', () => addExample(0));
+  document.getElementById('class-b').addEventListener('click', () => addExample(1));
+  document.getElementById('class-c').addEventListener('click', () => addExample(2));
 
    await setupWebcam();
    while (true) {
-    const result = await net.classify(webcamElement);
+    if (classifier.getNumClasses() > 0){
+      // get the mobilenet activation values from the webcam
+      const activation = net.infer(webcamElement, 'conv_preds');
+      
+      //  get the most likely class and confidences from the classifier
+      const result = await classifier.predictClass(activation);
 
-    document.getElementById('console').innerText = `
-        prediction: ${result[0].className}\n
-        probability: ${result[0].probability}
-    `;
+      const classes = ['A','C','B'];
+      document.getElementById('console').innerText = `
+        prediction: ${classes[result.classIndex]}\n
+        probability: ${result.confidences[result.classIndex]}
+      `;
+    }
+
 
     // Give some breathing room by waiting for the next animation frame to
     // fire.
