8bfea7f2e170da28caa7e63ac4f5cfb10b65d8a6
KenepAI <dijonkock@gmail.com>
2019-09-02 14:27:27 +0200

    6. Run MobileNet Inference in Browser through Webcam images

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let net;
+const webcamElement = document.getElementById('webcam');
 
 async function app() {
   console.log('Loading mobilenet..');
@@ -7,10 +8,49 @@ async function app() {
   net = await mobilenet.load(); 
   console.log('Sucessfully loaded model');
 
-  // Make a prediction through the model on our image.
-  const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
-  console.log(result);
+    // Make a prediction through the model on our image.
+    /* 
+    remove the prediction through the image and 
+    instead create an infinite loop which makes predictions through the webcam element.
+    const imgEl = document.getElementById('img');
+    const result = await net.classify(imgEl);
+    console.log(result);
+    */
+
+   await setupWebcam();
+   while (true) {
+    const result = await net.classify(webcamElement);
+
+    document.getElementById('console').innerText = `
+        prediction: ${result[0].className}\n
+        probability: ${result[0].probability}
+    `;
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
+    await tf.nextFrame();
+   }
 }
+
+//  webcam setup function
+async function setupWebcam() {
+    return new Promise((resolve, reject) => {
+      const navigatorAny = navigator;
+      navigator.getUserMedia = navigator.getUserMedia ||
+          navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+          navigatorAny.msGetUserMedia;
+      if (navigator.getUserMedia) {
+        navigator.getUserMedia({video: true},
+          stream => {
+            webcamElement.srcObject = stream;
+            webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+          },
+          error => reject());
+      } else {
+        reject();
+      }
+    });
+  }
+
 
 app();
